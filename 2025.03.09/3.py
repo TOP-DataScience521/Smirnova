e9c2b295b6b6a89ddbdf6a7afac179fc787a1953
ElenaneleS <elenaneles@gmail.com>
2025-03-16 16:16:10 +0300

    задача 3

---
+++ 2025.03.09/3.py
@@ -0,0 +1,12 @@
+all_min = int(input('введите целое число (минуты): '))
+haur = all_min //60
+min = all_min % 60
+print(f'\n{all_min} минут - это {haur} час. {min} мин.')
+
+
+
+# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.09
+ # 16:10:55 > python 3.py
+# введите целое число (минуты): 150
+
+# 150 минут - это 2 час. 30 мин.
