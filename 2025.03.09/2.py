fe4e275cec8d084cf0fa7c565872902a32186c01
ElenaneleS <elenaneles@gmail.com>
2025-03-16 13:59:34 +0300

    задача2

---
+++ 2025.03.09/2.py
@@ -0,0 +1,13 @@
+num1 = int(input('число: '))
+num2 = 1
+res1 = f'{num1 + num2}'
+res2 = f'{num1 - num2}'
+print('Следующее за числом', num1, 'число: ', res1, '\nДля числа', num1, 'предыдущее число: ', res2)
+
+
+
+# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.09
+ # 13:39:09 > python 2.py
+# число: 65
+# Следующее за числом 65 число:  66
+# Для числа 65 предыдущее число:  64
