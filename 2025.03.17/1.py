848bea71a35d2a2e779960e636b4018d621face2
ElenaneleS <elenaneles@gmail.com>
2025-03-22 22:51:11 +0300

    задача 1

---
+++ 2025.03.17/1.py
@@ -0,0 +1,28 @@
+a1 = float(input('число 1: '))
+b1 = float(input('число 2: '))
+c1 = float(input('число 3: '))
+
+if a1 > 0:
+    a2 = a1
+else:
+    a2 = 0
+    
+if b1 > 0:
+    b2 = b1
+else:
+    b2 = 0
+    
+if c1 > 0:
+    c2 = c1
+else:
+    c2 = 0
+    
+print(a2 + b2 + c2)
+
+
+# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
+ # 21:24:15 > python 1.py
+# число 1: 4
+# число 2: -7
+# число 3: 8.3
+# 12.3
