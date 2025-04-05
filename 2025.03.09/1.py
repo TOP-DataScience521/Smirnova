aaf96c0fe611b4dc229db7a4a167d1b5820a7b59
Gennadiy <renardes@gmail.com>
2025-04-05 22:33:33 +0500

    2025.03.09: оформление

--- 2025.03.09/1.py
+++ 2025.03.09/1.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
+
 name = input("Введите ваше имя: ")
 surname = input("Введите фамилию: ")
 birth_year = input("Введите год рождения: ")
 
-from datetime import datetime
 current_year = datetime.now().year
 print()
 
@@ -11,10 +13,9 @@ age = int(current_year) - int(birth_year)
 print(surname, name + ',', age)
 
 
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.09
- # 13:43:56 > python 1.py
 # Введите ваше имя: Елена
 # Введите фамилию: Смирнова
 # Введите год рождения: 1978
-
+# 
 # Смирнова Елена, 47
+
