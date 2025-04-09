480164b5a3c8365cb042db3955c4b88b9ecc893b
Gennadiy <renardes@gmail.com>
2025-04-09 22:38:20 +0500

    2025.03.17: проверено

--- 2025.03.17/3.py
+++ 2025.03.17/3.py
@@ -1,23 +1,23 @@
 year = int(input('введите год: '))
 
+# ИСПРАВИТЬ: избыточные круглые скобки
 if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0): 
     print('да') 
 else: 
     print('нет')
-    
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 14:57:08 > python 3.py
+
+
 # введите год: 2020
 # да
 
 
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 14:57:24 > python 3.py
 # введите год: 2000
 # да
 
 
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 14:57:31 > python 3.py
 # введите год: 1978
 # нет
+
+
+# ИТОГ: 3/4
+
--- 2025.03.17/4.py
+++ 2025.03.17/4.py
@@ -2,40 +2,42 @@ try:
     num = int(input('число: '))
 except ValueError:
     print('не является чиcлом')
-    exit()
-if num < 0 or num > 10:
-    print('нет')
+    # УДАЛИТЬ: избыточная операция
     exit()
 
-if num > 0 and (num & (num - 1))== 0:
-    
+# ИСПРАВИТЬ: эта и последующие операции должны быть выполнены только в случае успешного выполнения блока try, следовательно вам нужен блок else инструкции try
+if num < 0 or num > 10:
+    print('нет')
+    # УДАЛИТЬ: избыточная операция
+    exit()
+
+# ИСПРАВИТЬ: избыточные внешние круглые скобки 
+# КОММЕНТАРИЙ: очень изящное решение!
+if num > 0 and (num & (num - 1)) == 0:
+
 # res = num ** 0.5
 # if (res == int(res)) and (int(res) == num // 2):
-    
+
     print('да')
 else:
     print('нет')
-    
-    
-    # C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 19:29:44 > python 4.py
+
+
 # число: 4
 # да
 
 
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 19:30:02 > python 4.py
 # число: 1
 # да
 
 
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 19:30:28 > python 4.py
 # число: -8
 # нет
 
 
-# C:\Users\DmMSTR\Documents\DataScience521\Smirnova\2025.03.17
- # 19:30:32 > python 4.py
 # число: abc
 # не является чиcлом
+
+
+# ИТОГ: 3/5
+
